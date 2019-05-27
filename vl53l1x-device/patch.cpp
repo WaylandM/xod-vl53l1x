0d100f1f664c17831817d4bada86877df5e24ef3
Matt <mw283@cam.ac.uk>
2019-05-27 22:52:59 +0100

    new file:   get-measurement-timing-budget/patch.xodp 	new file:   range-status-to-string/patch.xodp 	new file:   read/patch.xodp 	new file:   set-distance-mode/patch.xodp 	new file:   set-measurement-timing-budget/patch.xodp 	new file:   start-continuous/patch.xodp 	new file:   stop-continuous/patch.xodp 	new file:   test/patch.xodp 	new file:   timeout-occurred/patch.xodp 	modified:   vl53l1x-device/patch.cpp

--- vl53l1x-device/patch.cpp
+++ vl53l1x-device/patch.cpp
@@ -8,12 +8,29 @@
 {{/global}}
 
 
+// Reserve memory to store an instance of the class,
+// and create the instance later:
 struct State {
+    uint8_t mem[sizeof(VL53L1X)];
 };
+
+// Define our custom type as a pointer on the class instance.
+using Type = VL53L1X*;
+
 
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
-    //auto inValue = getValue<input_IN>(ctx);
-    //emitValue<output_OUT>(ctx, inValue);
+    // It should be evaluated only once on the first (setup) transaction
+    if (!isSettingUp())
+        return;
+
+    auto state = getState(ctx);
+
+    // Create a new object in the memory area reserved previously.
+    Type sensor = new (state->mem) VL53L1X();
+    //address
+    //timeout
+
+    emitValue<output_DEV>(ctx, sensor);
 }
